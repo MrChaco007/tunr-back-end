23051fdf00400e49c84b7e14f24719805c724ab7
Jorge Soto <djsmoothzouk@gmail.com>
2021-02-04 14:01:21 -0600

    post, put, and delete routes added

--- controllers/songRoutes.js
+++ controllers/songRoutes.js
@@ -4,7 +4,7 @@ const Song = require('../models/song')
 
 //////// LIST ALL SONGS /////////////
 router.get('/', (req, res) => {
-    Song.find({}).populate('songs').then (allSongs => {
+    Song.find({}).then(allSongs => {
         res.json(allSongs)
     }).catch(err => res.json({status:400, err: err}))
 })
@@ -12,15 +12,34 @@ router.get('/', (req, res) => {
 /////// GET SONG BY TITLE //////////
 router.get('/:title', (req, res) => {
     Song.findOne({title: req.params.title})
-    .then((song) => res.json({status: 200, song: song})
+    .then((song) => res.json(song)
     )
     .catch(err => res.json({status:400, err: err}))
 })
 
 //////// CREATE A SONG /////////////
+router.post('/', (req, res) => {
+    const song = req.body
+    Song.create(song)
+    .then((song) => res.json({status: 200, data: song})
+    )
+    .catch(err => res.json({status:400, err: err}))
+})
 
 /////// UPDATE A SONG /////////////
+router.put('/updateSong/:id', (req, res) => {
+    Song.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then((song) => res.json({status: 200, data: song})
+    )
+    .catch(err => res.json({status:400, err: err}))
+})
 
 /////// DELETE A SONG ////////////
+router.delete('/:id', (req, res) => {
+    Song.findByIdAndDelete({})
+    .then((song) => res.json({status: 200, msg: 'Song has been deleted'})
+    )
+    .catch(err => res.json({status:400, err: err}))
+})
 
 module.exports = router
