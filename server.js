d5b76698f45397f00554bb868755a245b42d7acb
Alexander Chaconas <alexander.chaconas@gmail.com>
2021-02-03 20:19:25 -0500

    update

---
+++ server.js
@@ -0,0 +1,37 @@
+///////////////////////////
+// Environmental Variables
+///////////////////////////
+require("dotenv").config();
+const { PORT, NODE_ENV = "development" } = process.env;
+const mongoose = require("./db/connection");
+
+//CORS
+const cors = require("cors");
+//Bringing in Express
+const express = require("express");
+const app = express();
+
+//OTHER IMPORTS
+const morgan = require("morgan");
+
+
+////////////
+//MIDDLEWARE
+////////////
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan("tiny")); //logging
+
+///////////////
+//Routes and Routers
+//////////////
+app.get("/", (req, res) => {
+  res.json({ msg: "hello world" });
+});
+
+
+//LISTENER
+app.listen(PORT, () => {
+  console.log(`Your are listening on port ${PORT}`);
+});
